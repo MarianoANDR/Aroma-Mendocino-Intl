3ceacce106043652fb5d621904b9c83de5982f11
MarianoANDR <mariano.uk2022@gmail.com>
2023-01-03 10:31:50 +0000

    navbar and intro page

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+    return (
+        <div className="navBar">
+         <div>HOME</div>
+         <div>STORE</div>
+         <div className="imgNav"></div>
+         <div>EVENTS</div>
+         <div>CONTACT</div>
+        </div>
+    );
+}
+ 
+export default Navbar;
